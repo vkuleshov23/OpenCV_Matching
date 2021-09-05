342d40a5ed9342d69dea9af79e2fab11f1d8707f
Vadim Kuleshov <v.kuleshov@dev.local>
2021-09-06 01:08:04 +0300

    add conts | some visual and comfort for reading code

--- 3test.py
+++ 3test.py
@@ -24,9 +24,20 @@ average_points = []
 # аналогично, только хранятся расстояния average_sizes[0] соответвует average_pionts[0] | [1] == [1] | ...
 average_sizes = []
 #костанта, зачащая допустимую разницу размера, при котором не будет записываться новое значение
-# Пример: (точка 100, 100 не будет записыаться, если имеются точки: (100-difference, 100), (100, 100-difference), (100-difference, 100-difference), (100+difference, 100), ...) 
-difference = 20
-
+# Пример: (точка 100, 100 не будет записыаться, если имеются точки: (100-DIFFERENCE, 100), (100, 100-DIFFERENCE), (100-DIFFERENCE, 100-DIFFERENCE), (100+DIFFERENCE, 100), ...) 
+DIFFERENCE = 20
+#минимальный процент от размера шаблона
+MIN = 20
+#максимальный процент от размера шаблона (максимум - 100)
+MAX = 100
+#модуль разницы размеров шаблона
+MODULE = 5
+# Ядро свертки для размытия изображение (должно быть нечетным)
+BLUR_CORE_CONVULTION = (3, 3)
+#Имя файла для результатов
+FILE = "result.txt"
+# порог вхождения сопадений
+TRESHOLD = 0.8
 # Прочитайте шаблон
 template__ = cv2.imread(sys.argv[1] , 0)
 
@@ -46,9 +57,9 @@ def findAverage(array):
 #проверка на то, что точка еще в пределах 1го найденного изображения (из многих)
 def checkPixel(minPoint, point):
     
-    if point[0] > (minPoint[0] + difference):
+    if point[0] > (minPoint[0] + DIFFERENCE):
         return False
-    if point[1] > (minPoint[1] + difference):
+    if point[1] > (minPoint[1] + DIFFERENCE):
         return False
     return True
 
@@ -74,6 +85,13 @@ def clearAverageLists():
     average_sizes.clear()
     average_points.clear()
 
+def writeToFILE(data):
+    #запись результатов в файл
+    file = open(FILE, "w")
+    # print(getDataForFILE())
+    file.write(data)
+    file.close()
+
 #вырисовывание рамки объекта и ее положения 
 def print_result():
     i = 0
@@ -92,6 +110,7 @@ def getDataForFILE():
         string += str(average_points[i]) + ", " + str(average_sizes[i][0]) + ", " + str(average_sizes[i][1]) + "\n"
         i += 1
     return string
+
 # def rotation(template):
 #     for deg in range(0, 360):
 #         if deg % 5 == 0:
@@ -104,8 +123,11 @@ def getDataForFILE():
 #     return False
 
 #основная функция (вызывает другие), тут проходит уменщение шаблона и поиск шаблона на изображении
-def findingWithResize(MinPercent, module):
-    for size in range(MinPercent, 100):
+def findingWithResize(MinPercent, maxPercent, module):
+    if ((MinPercent < 0 or MinPercent > 100) or (maxPercent > 100 or maxPercent < 0) or (maxPercent < MinPercent)):
+        print("Wrong MIN, MAX sizes")
+        exit()
+    for size in range(MinPercent, maxPercent):
         #Ищем, только если шаблон увеличится на module процентов
         if size%module == 0:
             #нахождение ширины и высоты
@@ -127,12 +149,9 @@ def getContourse(template):
     # Выполнять операции сопоставления.
     res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
   
-    # порог нахождения
-    threshold = 0.8
-  
     # Сохранять координаты совпадающей области в массиве
     #координаты упорядочены по x, y (запись проходит вдоль икса)
-    loc = np.where( res >= threshold) 
+    loc = np.where( res >= TRESHOLD) 
     # флаг о том, что надо прекращать (для отладки)
     flag = False
 
@@ -154,7 +173,7 @@ def getContourse(template):
         #в ином случае, это другое изображение
         else:
             #добавляем старое в массив изображений
-            addAveragePoint(points, difference, w, h)
+            addAveragePoint(points, DIFFERENCE, w, h)
             #чистим список, для дальнейшей записи точек нового изображения
             points.clear()
             #устанавливаем точку как минимум для нового изобраения
@@ -166,7 +185,7 @@ def getContourse(template):
         # flag = True
     
     if flag_about_in == True:    
-        addAveragePoint(points, difference, w, h)
+        addAveragePoint(points, DIFFERENCE, w, h)
     return flag
 
 stop_flag = False
@@ -177,20 +196,17 @@ while True:
     #сделать снимок экрана
     img_rgb = vs.read()
     #добавление размытости
-    img_rgb = cv2.GaussianBlur(img_rgb, (3, 3), 0)
+    img_rgb = cv2.GaussianBlur(img_rgb, BLUR_CORE_CONVULTION , 0)
     #преобразовать изображение в оттенки серого
     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
     #условие для отладки
     if stop_flag == False:
         #поиск всех совпадений для изображения
-        stop_flag = findingWithResize(20, 5)
+        stop_flag = findingWithResize(MIN, MAX, MODULE)
         #нарисовка рамок и координат
         print_result()
-        #запись результатов в файл
-        file = open('result.txt', "w")
-        print(getDataForFILE())
-        file.write(getDataForFILE())
-        file.close()
+        #Запись в файл
+        writeToFILE(getDataForFILE())
         # Показать окончательное изображение с соответствующей области.
         #отчиска списка (для отладки)
         clearAverageLists()
